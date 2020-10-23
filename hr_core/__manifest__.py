a915911f3a4c24c3e93935e5aec11a62787501aa
Dany FAHED <daf@everteam-mea.com>
2020-10-23 13:41:13 +0300

    depend issues

--- hr_core/__manifest__.py
+++ hr_core/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'hr', 'web', 'security_rules'],
+    'depends': ['base', 'hr', 'web'],
 
     # always loaded
     'data': [
